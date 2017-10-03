344b38f97998357979e606341ca66eb77317b28b
Matthew <mattmuelle@gmail.com>
2017-10-03 14:46:08 +0700

    added retry + timeout

--- index.js
+++ index.js
@@ -8,9 +8,7 @@ var Deferred = require('deferral')
  * Async loader on the browser-side
  */
 
-var Load = typeof window !== 'undefined'
-  ? require('./load')
-  : function () {}
+var Load = typeof window !== 'undefined' ? require('./load') : function() {}
 
 /**
  * Export `Stripe`
@@ -22,32 +20,55 @@ module.exports = Stripe
  * Initialize `Stripe`
  */
 
-function Stripe (setup, script) {
+function Stripe(setup, script) {
   setup = setup || {}
   script = script || 'https://checkout.stripe.com/checkout.js'
 
-  const loading = new Deferred()
-  Load && Load(script, function (err) {
-    return err ? loading.reject(err) : loading.resolve()
+  const loading = tryLoading(script, new Deferred(), 5)
+
+  return function stripe(params) {
+    if (!Load)
+      return Promise.reject('This module is only supported on the browser-side')
+    const complete = new Deferred()
+    const config = Object.assign(
+      {
+        token: function(token) {
+          complete.resolve(token)
+        },
+        closed: function() {
+          complete.resolve(false)
+        }
+      },
+      setup,
+      params
+    )
+
+    return loading.then(function() {
+      const handler = window.StripeCheckout.configure(config)
+      handler.open(config)
+      return complete
+    })
+  }
+}
+
+function tryLoading(script, deferred, retry) {
+  if (retry <= 0) {
+    return deferred.reject('timeout expired')
+  }
+
+  var loaded = false
+  Load(script, function(err) {
+    if (err) {
+      return tryLoading(script, deferred, retry - 1)
+    }
+    loaded = true
+    return deferred.resolve()
   })
 
-  return function stripe (params) {
-    if (!Load) return Promise.reject('This module is only supported on the browser-side')
-    const complete = new Deferred()
-    const config = Object.assign({
-      token: function (token) {
-        complete.resolve(token)
-      },
-      closed: function () {
-        complete.resolve(false)
-      }
-    }, setup, params)
+  setTimeout(function() {
+    if (loaded) return deferred
+    tryLoading(script, deferred, retry - 1)
+  }, 10000)
 
-    return loading
-      .then(function () {
-        const handler = window.StripeCheckout.configure(config)
-        handler.open(config)
-        return complete
-      })
-  }
+  return deferred
 }
